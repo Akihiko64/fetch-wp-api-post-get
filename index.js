7dababeaf357b31de34492f9d0ac891948471a06
Nacelio Barbosa <naceliombdev@gmail.com>
2021-09-06 21:53:37 -0300

    script for POST and GET api wordpress example

--- index.js
+++ index.js
@@ -1,3 +1,74 @@
-import fetch from 'node-fetch';
+import fetch from "node-fetch";
+import { base64encode, base64decode } from "nodejs-base64";
 
+async function requestPosts() {
+	const response = await fetch(
+		"https://rafaelasalvato.com.br/wp-json/wp/v2/procedimentos?per_page=100",
+		{
+			mode: "cors",
+			method: "GET",
+			headers: {
+				Accept: "application/json",
+			},
+		}
+	);
 
+	const body = await response.json();
+	return body;
+}
+
+async function getContentPosts() {
+	const content = await requestPosts();
+
+	let postsPublish = [];
+
+	content.forEach((content) => {
+		if (content.status == "publish") {
+			let item = new Object();
+			// item.id = content.id;
+			// item.date = content.date;
+			// item.slug = content.slug;
+			item.title = content.title.rendered;
+			item.content = content.content.rendered;
+			item.status = content.status;
+			postsPublish.push(item);
+
+			// console.log(item);
+		}
+	});
+
+	postsPublish.forEach(async (postsPublish) => {
+		await createPostTest(postsPublish);
+	});
+}
+
+getContentPosts();
+//robotCreator
+
+async function createPostTest(content) {
+	const response = await fetch(
+		"https://devscripta.com.br/wp-json/wp/v2/posts",
+		{
+			mode: "cors",
+			method: "POST",
+			headers: {
+				Accept: "application/json",
+				"Content-Type": "application/json;charset=UTF-8",
+				Authorization: "Basic " + base64encode("user:password"),
+			},
+
+			// body: JSON.stringify({
+			// 	"title": "Post Example",
+			// 	"content": "Hello World",
+			// 	"excerpt": "example",
+			// 	"status": "publish"
+			// }),
+			body: JSON.stringify(content),
+		}
+	);
+
+	const body = await response.json();
+	console.log(body);
+}
+
+// createPostTest();
